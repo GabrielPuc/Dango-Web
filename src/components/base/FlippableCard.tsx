7ecc5bf4ab925909d3f61d9f5f605c6d3e0d341e
tonyalonzoc <tonyalonzoc@gmail.com>
2024-04-24 00:36:25 -0600

    Fix card color and code formatting issue

--- src/components/base/FlippableCard.tsx
+++ src/components/base/FlippableCard.tsx
@@ -1,23 +1,35 @@
-import {useState} from 'react';
-import {motion} from 'framer-motion';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
 
-const FlippableCard = ({front, back}) => {
-    const [isFlipped, setIsFlipped] = useState(false);
+const FlippableCard = ({ front, back }) => {
+  const [isFlipped, setIsFlipped] = useState(false);
+  const [isAnimating, setIsAnimating] = useState(false);
 
-    function handleFlip() {
-        setIsFlipped(!isFlipped);
+  function handleFlip() {
+    if (!isAnimating) {
+      setIsFlipped(!isFlipped);
+      setIsAnimating(true);
     }
+  }
 
-    return (<div className="flip-card w-[300px] h-[400px]" onClick={handleFlip}>
-        <motion.div className="flip-card-inner text-black w-[100%] h-[100%] rounded-md" initial={false} animate={{rotateY: isFlipped ? 180 : 360}} transition={{duration: 0.6}}>
-            <div className="flip-card-front w-[100%] h-[100%] bg-stone-300 flex justify-center items-center rounded-lg">
-                {front}
-            </div>
-            <div className="flip-card-back w-[100%] h-[100%] bg-stone-300 flex justify-center items-center rounded-lg">
-                {back}
-            </div>
-        </motion.div>
-    </div>);
-}
+  return (
+    <div className="flip-card w-[400px] h-[400px]" onClick={handleFlip}>
+      <motion.div
+        className="flip-card-inner text-white w-[100%] h-[100%] rounded-md"
+        initial={false}
+        animate={{ rotateY: isFlipped ? 180 : 360 }}
+        transition={{ duration: 0.6 }}
+        onAnimationComplete={() => setIsAnimating(false)}
+      >
+        <div className="flip-card-front w-[100%] h-[100%] bg-[#292929] flex justify-center items-center rounded-lg">
+          {front}
+        </div>
+        <div className="flip-card-back w-[100%] h-[100%] bg-[#292929] flex justify-center items-center rounded-lg">
+          {back}
+        </div>
+      </motion.div>
+    </div>
+  );
+};
 
 export default FlippableCard;
