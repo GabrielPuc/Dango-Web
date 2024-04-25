94c849040f7e32b1cecf6a3990e85df45e62eebe
tonyalonzoc <tonyalonzoc@gmail.com>
2024-04-24 23:06:15 -0600

    Rename Quiz component and add on click functionality

---
+++ src/components/base/QuizOption.tsx
@@ -0,0 +1,37 @@
+import { motion } from 'framer-motion';
+
+const QuizOption = ({
+  index,
+  option,
+  selectedIndex,
+  answerIndex,
+  handleClick,
+}) => {
+  const isSelected = selectedIndex === index;
+  const isCorrect = index === answerIndex;
+
+  let backgroundColor = '#133952';
+
+  if (selectedIndex != null) {
+    if (isCorrect) {
+      backgroundColor = '#7B9D45';
+    } else if (isSelected) {
+      backgroundColor = '#8D0A0A';
+    }
+  }
+
+  return (
+    <motion.button
+      animate={{
+        backgroundColor: backgroundColor,
+      }}
+      transition={{ duration: 0.2 }}
+      onClick={handleClick}
+      className={`rounded overflow-hidden shadow-lg px-8 py-6 font-bold text-xl mb-2 text-white text-center uppercase ${selectedIndex != null && 'pointer-events-none'}`}
+    >
+      {option}
+    </motion.button>
+  );
+};
+
+export default QuizOption;
--- src/components/base/QuizzOption.tsx
+++
@@ -1,9 +0,0 @@
-const QuizzOption = ({ option }) => {
-  return (
-    <button className="bg-[#133952] rounded overflow-hidden shadow-lg px-8 py-6 font-bold text-xl mb-2 text-white text-center uppercase">
-      {option}
-    </button>
-  );
-};
-
-export default QuizzOption;
