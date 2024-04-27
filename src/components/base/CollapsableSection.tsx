dae4cd2d24d2e9afa4dcbfdd4e89f50f2884cb6d
GabrielPuc <gabriel.puc@outlook.com>
2024-04-26 19:55:15 -0600

    Added filter option in Verbs Page

--- src/components/base/CollapsableSection.tsx
+++ src/components/base/CollapsableSection.tsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
 import { AiFillCaretRight, AiFillCaretDown } from 'react-icons/ai';
 
-export const CollapsableSection = ({ title, content, expanded = false }) => {
+export const CollapsableSection = ({
+  title,
+  content,
+  expanded = false,
+  visible = true,
+}) => {
   const [isExpanded, setExpanded] = useState(expanded);
 
   return (
-    <div>
+    <div className={visible ? 'visible' : 'collapse h-0'}>
       <div onClick={() => setExpanded(!isExpanded)}>
         <div className="bg-green flex justify-between text-xl text-white px-2 py-2 mt-2">
           {title}
--- src/components/pages/VerbList.tsx
+++ src/components/pages/VerbList.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import {
   getVerbsSelector,
   getVerbsPendingSelector,
@@ -12,21 +13,38 @@ function VerbList() {
   const verbGroups = useSelector(getVerbsSelector);
   const error = useSelector(getVerbsErrorSelector);
 
-  console.log(verbGroups);
+  const [filter, setFilter] = useState('');
+
+  const filteredVerbs = verbGroups.map((element) => {
+    return {
+      ...element,
+      content: element.content.filter((subElement) =>
+        subElement.meaning.toLowerCase().includes(filter.toLowerCase())
+      ),
+    };
+  });
 
   return (
     <div className="bg-slate flex justify-center">
       <div className="w-screen h-max max-w-screen-lg">
+        <input
+          type="text"
+          placeholder="Search by meaning"
+          id="filter"
+          className="bg-dark border border-pinkBright text-white text-l rounded-lg focus:ring-pinkBright focus:border-pinkBright block w-screen h-max max-w-screen-lg mt-4 p-2.5"
+          onChange={(e) => setFilter(e.target.value)}
+        />
         {pending ? (
           <div>Loading...</div>
         ) : error ? (
           <div>Error</div>
         ) : (
           <div>
-            {verbGroups.map((option) => (
+            {filteredVerbs.map((option) => (
               <CollapsableSection
                 title={option.name}
                 expanded={option.content.length == 1}
+                visible={option.content.length != 0}
                 content={
                   <div className="h-max grid sm:w-screen sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 max-w-screen-lg p-4 mb-10">
                     {option.content.map((item, index) => (
