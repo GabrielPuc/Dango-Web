eb8192e28bbd9dd359a67e5a0d928570fc34a786
GabrielPuc <gabriel.puc@outlook.com>
2024-05-10 17:57:39 -0600

    FAB is not hidden for mobile, needs alternative

--- src/components/blocks/NavigationFAB.tsx
+++ src/components/blocks/NavigationFAB.tsx
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 export default function NavigationFAB() {
   const navigate = useNavigate();
   return (
-    <div className="fixed bottom-1/4">
+    <div className="fixed bottom-1/4 hidden md:flex">
       <button
         onClick={() =>
           window.scrollTo({
